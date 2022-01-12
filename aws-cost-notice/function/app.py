2dc23aeaaf845bd8ee7d853c6b2e2ae321aeb71e
yokobot <takuou@gmail.com>
2022-01-12 23:07:56 +0900

    料金取得メソッド追加

--- aws-cost-notice/function/app.py
+++ aws-cost-notice/function/app.py
@@ -1,10 +1,19 @@
 # coding; utf-8
-import json
+import boto3
+
+def get_aws_cost():
+    ce = boto3.client('ce')
+    response = ce.get_cost_and_usage(
+        TimePeriod={
+            'Start': '2022-01-10',
+            'End': '2022-01-11'
+        },
+        Granularity='DAILY',
+        Metrics=[
+            'BlendedCost',
+        ]
+    )
+    print(response)
 
 def lambda_handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-        }),
-    }
+    get_aws_cost()
