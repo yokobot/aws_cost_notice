5878e7d024548e184f1dfa6049032cbd11c14a6c
yokobot <takuou@gmail.com>
2022-01-15 20:15:54 +0900

    chat_postMessageに変更

--- aws-cost-notice/function/app.py
+++ aws-cost-notice/function/app.py
@@ -1,9 +1,19 @@
 # coding; utf-8
+import logging
+import os
 import boto3
-from slack_sdk.web import WebClient
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+
+logger = logging.getLogger(__name__)
+
+
+web_client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
+ce = boto3.client('ce')
+
 
 def get_aws_cost():
-    ce = boto3.client('ce')
     response = ce.get_cost_and_usage(
         TimePeriod={
             'Start': '2022-01-10',
@@ -17,9 +27,19 @@ def get_aws_cost():
     print(response)
 
 def send_to_slack():
-    client = WebClient()
-    response = client.api_test()
-    print(response)
+    # ID of the channel you want to send the message to
+    channel_id = os.environ.get("SLACK_CHANNEL_ID")
+
+    try:
+        # Call the chat.postMessage method using the WebClient
+        result = web_client.chat_postMessage(
+            channel=channel_id,
+            text="Hello world"
+        )
+        logger.info(result)
+
+    except SlackApiError as e:
+        logger.error(f"Error posting message: {e}")
 
 def lambda_handler(event, context):
     get_aws_cost()
